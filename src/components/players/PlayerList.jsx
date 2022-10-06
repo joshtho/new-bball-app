ff994c2d29593773fd130f26e0fb0b29df9804be
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-10-05 19:09:40 -0500

    populate the PlayerPage

--- src/components/players/PlayerList.jsx
+++ src/components/players/PlayerList.jsx
@@ -3,12 +3,12 @@ import Accordion from 'react-bootstrap/Accordion';
 import PlayerPage from './PlayerPage';
 
 function PlayerList({players}) {
-    const [stats, setStats] = useState([])
+    const [stats, setStats] = useState({})
 
     function handleClick(id) {
         fetch(`https://www.balldontlie.io/api/v1/season_averages?season=2021&player_ids[]=${id}`)
             .then(r => r.json())
-            .then(data => setStats(data))
+            .then(info => setStats(info.data[0]))
 
     }
     
@@ -19,7 +19,7 @@ function PlayerList({players}) {
             <Accordion.Item eventKey={`${index}`} key={player.id} onClick={() => handleClick(player.id)}>
               <Accordion.Header>{player.first_name + " " + player.last_name}</Accordion.Header>
               <Accordion.Body>
-                {<PlayerPage player={player} />}
+                {<PlayerPage player={player} stats={stats} />}
               </Accordion.Body>
             </Accordion.Item> 
 
--- src/components/players/PlayerPage.jsx
+++ src/components/players/PlayerPage.jsx
@@ -3,10 +3,10 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 
-function PlayerPage({player}) {
-//const [hide, setHide] = useState(true)
+function PlayerPage({player, stats}) {
+const [hide, setHide] = useState(true)
 
-
+console.log(stats)
     
 
 // function handleClick(id) { 
@@ -20,57 +20,56 @@ function PlayerPage({player}) {
 
 // }
 
-//const {pts, reb, ast, min, stl, blk, ft_pct, fg_pct, fg3_pct, fg3m} = stats
+const {pts, reb, ast, min, stl, blk, ft_pct, fg_pct, fg3_pct, fg3m} = stats
 // const min = parseInt(stats.min)
 
 return (
     <>
     {
-    // players ? players.map(player => (
-    //      <Card key={player.id}>
-    //       <Card.Header>{player.position}</Card.Header>
-    //       <Card.Body>
-    //         <Card.Title>{player.first_name + " " + player.last_name}</Card.Title>
-    //         <Card.Text>
-    //         {player.team ? player.team.city : "Loading..."}
-    //         <br />
-    //         {player.team ? player.team.name : "Loading..."}
-    //         </Card.Text>
-    //         {
-    //             hide ? 
-    //             <Button variant="primary" onClick={() => setHide(false)}>2021 Stats</Button>
-    //             : 
-    //             <>
-    //             { stats ? 
-    //             <Card.Text>
-    //                 Points: {pts}
-    //                 <br/>
-    //                 Rebounds: {reb}
-    //                 <br/>
-    //                 Assists: {ast}
-    //                 <br/>
-    //                 Minutes: {min}
-    //                 <br/>
-    //                 Steals: {stl}
-    //                 <br/>
-    //                 Blocks: {blk}
-    //                 <br/>
-    //                 FG%: {fg_pct}
-    //                 <br/>
-    //                 FT%: {ft_pct}
-    //                 <br/>
-    //                 3P%: {fg3_pct}
-    //                 <br/>
-    //                 3PM: {fg3m}
-    //             </Card.Text>
-    //             :
-    //             "Loading..."
-    //             }
-    //             </>
-    //         }
-    //       </Card.Body>
-    //     </Card>)) : <h1>Loading...</h1>
-        <h1>Im the Player Page</h1>
+    player ? 
+         <Card key={player.id}>
+          <Card.Header>{player.position}</Card.Header>
+          <Card.Body>
+            <Card.Title>{player.first_name + " " + player.last_name}</Card.Title>
+            <Card.Text>
+            {player.team ? player.team.city : "Loading..."}
+            <br />
+            {player.team ? player.team.name : "Loading..."}
+            </Card.Text>
+            {
+                hide ? 
+                <Button variant="primary" onClick={() => setHide(false)}>2021 Stats</Button>
+                : 
+                <>
+                { stats ? 
+                <Card.Text>
+                    Points: {pts}
+                    <br/>
+                    Rebounds: {reb}
+                    <br/>
+                    Assists: {ast}
+                    <br/>
+                    Minutes: {min}
+                    <br/>
+                    Steals: {stl}
+                    <br/>
+                    Blocks: {blk}
+                    <br/>
+                    FG%: {fg_pct}
+                    <br/>
+                    FT%: {ft_pct}
+                    <br/>
+                    3P%: {fg3_pct}
+                    <br/>
+                    3PM: {fg3m}
+                </Card.Text>
+                :
+                "Loading..."
+                }
+                </>
+            }
+          </Card.Body>
+        </Card> : <h1>Loading...</h1>
     }
 
     
